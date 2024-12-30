718ada7ac57eb8aa674aecff3d27399ff7752064
ReyanGG <reyangupta1078@gmail.com>
2024-12-30 18:55:16 +0530

    acticity done

---
+++ Calci.java
@@ -0,0 +1,18 @@
+public class Calci {
+    public static void main(String[] args) {
+        int n = 50;
+        int m = 50;
+        int s = m + n;
+        int d = m - n;
+        int p = m * n;
+        int q = m / n;
+        int r = m % n;
+
+        System.out.println("sum is " + s);
+        System.out.println("diff is " + d);
+        System.out.println("product is " + p);
+        System.out.println("quotient is " + q);
+        System.out.println("reminder is " + r);
+
+    }
+}
