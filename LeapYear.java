5abf93907841dae185d2ffa170fa52368750a0b9
ReyanGG <reyangupta1078@gmail.com>
2025-01-03 18:35:43 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,10 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int a = 1964;
+        if (a % 4 == 0) {
+            System.out.println("It is a leap year");
+        } else {
+            System.out.println("It is not a leap year");
+        }
+    }
+}
